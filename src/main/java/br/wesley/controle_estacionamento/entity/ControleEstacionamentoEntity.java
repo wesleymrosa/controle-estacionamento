c4f447c4f2cb993f0f4f61df7cafcdd63eb40033
Wesley Rosa <wesleymrosa@gmail.com>
2025-03-13 16:49:18 -0300

    Foi criada a entidade.

--- src/main/java/br/wesley/controle_estacionamento/entity/ControleEstacionamentoEntity.java
+++ src/main/java/br/wesley/controle_estacionamento/entity/ControleEstacionamentoEntity.java
@@ -1,4 +1,16 @@
 package br.wesley.controle_estacionamento.entity;
 
-public class ControleEstacionamentoEntity {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import java.io.Serializable;
+
+@Entity
+public class ControleEstacionamentoEntity implements Serializable {
+
+    private static final long serializable = 1L;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
 }

6b368b37c8fc1666e0fc13c1010d800ed9739821
Wesley Rosa <wesleymrosa@gmail.com>
2025-03-17 15:08:57 -0300

    Inseridos os atributos da entidade.

--- src/main/java/br/wesley/controle_estacionamento/entity/ControleEstacionamentoEntity.java
+++ src/main/java/br/wesley/controle_estacionamento/entity/ControleEstacionamentoEntity.java
@@ -1,16 +1,42 @@
 package br.wesley.controle_estacionamento.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "tb_controle_estacionamento")
 public class ControleEstacionamentoEntity implements Serializable {
-
     private static final long serializable = 1L;
+    @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(nullable = false, unique = true, length = 10)
+    private String numeroDaVaga;
+
+    @Column(nullable = false, unique = true, length = 7)
+    private String placaDoCarro;
+
+    @Column(nullable = false, length = 70)
+    private String marcaDoCarro;
+
+    @Column(nullable = false, length = 70)
+    private String modeloDoCarro;
+
+    @Column(nullable = false, length = 70)
+    private String corDoCarro;
+
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+
+    @Column(nullable = false, length = 130)
+    private String responsibleName;
+
+    @Column(nullable = false, length = 30)
+    private String apartment;
+
+    @Column(nullable = false, length = 30)
+    private String block;
 }

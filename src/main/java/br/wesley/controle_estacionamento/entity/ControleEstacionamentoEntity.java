823250066a643f9d5969c22e206d99d1d799eec9
Wesley rosa <wesleymrosa@gmail.com>
2025-03-18 12:16:02 -0300

    Inseridos os contrutores e os getters e setters na entidade.

--- src/main/java/br/wesley/controle_estacionamento/entity/ControleEstacionamentoEntity.java
+++ src/main/java/br/wesley/controle_estacionamento/entity/ControleEstacionamentoEntity.java
@@ -29,14 +29,112 @@ public class ControleEstacionamentoEntity implements Serializable {
     private String corDoCarro;
 
     @Column(nullable = false)
-    private LocalDateTime registrationDate;
+    private LocalDateTime dataDeRegistro;
 
     @Column(nullable = false, length = 130)
-    private String responsibleName;
+    private String nomeDoResponsavel;
 
     @Column(nullable = false, length = 30)
-    private String apartment;
+    private String apartamento;
 
     @Column(nullable = false, length = 30)
-    private String block;
+    private String torre;
+
+    public ControleEstacionamentoEntity() {
+    }
+
+    public ControleEstacionamentoEntity(Long id, String numeroDaVaga, String placaDoCarro, String marcaDoCarro,
+                                        String modeloDoCarro, String corDoCarro, LocalDateTime dataDeRegistro,
+                                        String nomeDoResponsavel, String apartamento, String torre) {
+        this.id = id;
+        this.numeroDaVaga = numeroDaVaga;
+        this.placaDoCarro = placaDoCarro;
+        this.marcaDoCarro = marcaDoCarro;
+        this.modeloDoCarro = modeloDoCarro;
+        this.corDoCarro = corDoCarro;
+        this.dataDeRegistro = dataDeRegistro;
+        this.nomeDoResponsavel = nomeDoResponsavel;
+        this.apartamento = apartamento;
+        this.torre = torre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumeroDaVaga() {
+        return numeroDaVaga;
+    }
+
+    public void setNumeroDaVaga(String numeroDaVaga) {
+        this.numeroDaVaga = numeroDaVaga;
+    }
+
+    public String getPlacaDoCarro() {
+        return placaDoCarro;
+    }
+
+    public void setPlacaDoCarro(String placaDoCarro) {
+        this.placaDoCarro = placaDoCarro;
+    }
+
+    public String getMarcaDoCarro() {
+        return marcaDoCarro;
+    }
+
+    public void setMarcaDoCarro(String marcaDoCarro) {
+        this.marcaDoCarro = marcaDoCarro;
+    }
+
+    public String getModeloDoCarro() {
+        return modeloDoCarro;
+    }
+
+    public void setModeloDoCarro(String modeloDoCarro) {
+        this.modeloDoCarro = modeloDoCarro;
+    }
+
+    public String getCorDoCarro() {
+        return corDoCarro;
+    }
+
+    public void setCorDoCarro(String corDoCarro) {
+        this.corDoCarro = corDoCarro;
+    }
+
+    public LocalDateTime getDataDeRegistro() {
+        return dataDeRegistro;
+    }
+
+    public void setDataDeRegistro(LocalDateTime dataDeRegistro) {
+        this.dataDeRegistro = dataDeRegistro;
+    }
+
+    public String getNomeDoResponsavel() {
+        return nomeDoResponsavel;
+    }
+
+    public void setNomeDoResponsavel(String nomeDoResponsavel) {
+        this.nomeDoResponsavel = nomeDoResponsavel;
+    }
+
+    public String getApartamento() {
+        return apartamento;
+    }
+
+    public void setApartamento(String apartamento) {
+        this.apartamento = apartamento;
+    }
+
+    public String getTorre() {
+        return torre;
+    }
+
+    public void setTorre(String torre) {
+        this.torre = torre;
+    }
 }

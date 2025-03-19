9916fcc1590067ce1ac2877ece42623f96854841
Wesley Rosa <wesleymrosa@gmail.com>
2025-03-19 08:42:52 -0300

    Criado o método salvar na Service.

--- src/main/java/br/wesley/controle_estacionamento/services/ControleEstacionamentoService.java
+++ src/main/java/br/wesley/controle_estacionamento/services/ControleEstacionamentoService.java
@@ -1,7 +1,21 @@
 package br.wesley.controle_estacionamento.services;
 
+import br.wesley.controle_estacionamento.entity.ControleEstacionamentoEntity;
+import br.wesley.controle_estacionamento.repositories.ControleEstacionamentoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ControleEstacionamentoService {
+
+    private final ControleEstacionamentoRepository controleEstacionamentoRepository;
+
+    public ControleEstacionamentoService(ControleEstacionamentoRepository controleEstacionamentoRepository) {
+        this.controleEstacionamentoRepository = controleEstacionamentoRepository;
+    }
+
+    public ControleEstacionamentoEntity salvar (ControleEstacionamentoEntity controleEstacionamentoEntity) {
+        return controleEstacionamentoRepository.save(controleEstacionamentoEntity);
+    }
+
+
 }

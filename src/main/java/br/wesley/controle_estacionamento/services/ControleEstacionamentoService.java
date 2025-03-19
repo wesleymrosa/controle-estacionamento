1e3bd57f074f0e3041f0ba7d7c137aa54765c220
Wesley Rosa <wesleymrosa@gmail.com>
2025-03-19 08:52:01 -0300

    Inserido o método listarTudo no Service.

--- src/main/java/br/wesley/controle_estacionamento/services/ControleEstacionamentoService.java
+++ src/main/java/br/wesley/controle_estacionamento/services/ControleEstacionamentoService.java
@@ -4,6 +4,8 @@ import br.wesley.controle_estacionamento.entity.ControleEstacionamentoEntity;
 import br.wesley.controle_estacionamento.repositories.ControleEstacionamentoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ControleEstacionamentoService {
 
@@ -13,9 +15,11 @@ public class ControleEstacionamentoService {
         this.controleEstacionamentoRepository = controleEstacionamentoRepository;
     }
 
-    public ControleEstacionamentoEntity salvar (ControleEstacionamentoEntity controleEstacionamentoEntity) {
+    public ControleEstacionamentoEntity salvar(ControleEstacionamentoEntity controleEstacionamentoEntity) {
         return controleEstacionamentoRepository.save(controleEstacionamentoEntity);
     }
 
-
+    public List<ControleEstacionamentoEntity> listarTudo() {
+        return controleEstacionamentoRepository.findAll();
+    }
 }

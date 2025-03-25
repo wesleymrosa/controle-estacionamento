bb2a22cad493fef082ecade5a0a94ea3bec6686f
Wesley Rosa <wesleymrosa@gmail.com>
2025-03-25 17:06:31 -0300

    Inserido o pacote configuracoes e a classe ConfiguracaoDeData para aplicar formato global de data ISO 8601 com sufixo Z para LocalDateTime no JSON

---
+++ src/main/java/br/wesley/controle_estacionamento/configuracoes/ConfiguracaoDeData.java
@@ -0,0 +1,77 @@
+package br.wesley.controle_estacionamento.configuracoes;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+@Configuration
+public class ConfiguracaoDeData {
+
+    private static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    private static final LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER =
+            new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customDateFormat() {
+        return builder -> builder
+                .serializers(LOCAL_DATETIME_SERIALIZER);
+    }
+}
+
+
+//package br.wesley.controle_estacionamento.configuracoes;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+//import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//import java.time.format.DateTimeFormatter;
+//
+//@Configuration
+//public class ConfiguracaoDeData {
+//
+//    public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+//    public static final LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER =
+//            new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+//
+//    @Bean
+//    @Primary
+//    public ObjectMapper objectMapper() {
+//        JavaTimeModule module = new JavaTimeModule();
+//        module.addSerializer(LOCAL_DATETIME_SERIALIZER);
+//        return new ObjectMapper()
+//                .registerModule(module);
+//    }
+//}
+
+
+//package br.wesley.controle_estacionamento.configuracoes;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+//import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Primary;
+//
+//import java.time.format.DateTimeFormatter;
+//
+//public class ConfiguracaoDeData {
+//    public static final String DATETIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+//    public static final LocalDateTimeSerializer LOCAL_DATETIME_SERIALIZER =
+//            new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT));
+//
+//    @Bean
+//    @Primary
+//    public ObjectMapper objectMapper() {
+//        JavaTimeModule module = new JavaTimeModule();
+//        module.addSerializer(LOCAL_DATETIME_SERIALIZER);
+//        return new ObjectMapper()
+//                .registerModule(module);
+//    }
+//}
